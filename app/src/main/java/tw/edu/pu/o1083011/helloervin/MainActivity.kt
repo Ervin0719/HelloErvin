dc77ae9ca5d3a16d8be5cdad2dbefa151575ff95
student <o1083026@gm.pu.edu.tw>
2021-03-15 14:33:41 +0800

    我是嘉慧

--- app/src/main/java/tw/edu/pu/o1083011/helloervin/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083011/helloervin/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello 趙泓斌"
+        txv.text = "Hello 趙泓斌 我是嘉慧"
         TestDEV()
     }
 
